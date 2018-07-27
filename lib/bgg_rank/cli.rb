da209e54b216d4084ceccdd0b3cd4e61c63bc8d4
corytveten <corytveten@gmail.com>
2018-07-27 00:53:21 +0000

    updated menu stubs

--- lib/bgg_rank/cli.rb
+++ lib/bgg_rank/cli.rb
@@ -1,7 +1,38 @@
 class BggRank::CLI
 
   def call
-    puts "BGG 100"
+    list_games
+    menu
+    goodbye
   end
 
+  def list_games
+    puts "BGG 100"
+    puts "1. Gloomhaven
+    2. Pandemic Legacy".gsub /^\s*/, ''
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the game for more info."
+      puts "Type exit to leave."
+      puts "Type menu for menu."
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "more info on Gloomhaven."
+      when "2"
+        puts "more info on Pandemic Legacy"
+      when "menu"
+        list_games
+      else
+        puts "Not sure what you want."
+      end
+    end
+  end
+
+  def goodbye
+    puts 'Goodbye!'
+  end
 end
